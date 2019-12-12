0786f1c579f816fd90ffd28c9e025128d5f1d1ba
Adrian Rodriguez <adrianwritenow@gmail.com>
2019-12-12 14:05:27 -0500

    basic navigation added to cards

--- lib/catagories_screen.dart
+++ lib/catagories_screen.dart
@@ -6,10 +6,19 @@ class CatagoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(title: const Text('DeliMeal'),),
-          body: GridView(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.title, catData.color))
+            .map(
+              (catData) => CategoryItem(
+                catData.id,
+                catData.title,
+                catData.color,
+              ),
+            )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
